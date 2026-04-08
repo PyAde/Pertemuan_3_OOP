6f97156bc6e0dcbf394b44346d12022856aaf3b8
PyAde <putraadelio26@gmail.com>
2026-04-08 17:05:59 +0800

    .

--- Main.java
+++ Main.java
@@ -7,6 +7,16 @@ public class Main {
         if(loginBerhasil){
             System.err.println("Login atas nama " + user1.getUsername() + " berhasil");
         
+            // Validasi kendaraan;
+            PencocokanKendaraan transport_1 = new PencocokanKendaraan("012345", "AB-123", "BC-123", "CD-123");
+            boolean validasiBerhasil = transport_1.validasiData();
+            if(validasiBerhasil){
+                System.out.println("Validasi kendaraan atas nama " + user1.getUsername() + "berhasil dilakukan");
+            }else{
+                System.out.println("Validasi kendaraan atas nama" + user1.getUsername() + "tidak berhasil dilakukan");
+            }
+            // lanjut ke proses class diagram berikutnya 
+
         
         user1.logout();
         }else{
---
+++ PencocokanKendaraan.java
@@ -0,0 +1,33 @@
+public class PencocokanKendaraan {
+    private final String NIK;
+    private final String nomerMesin;
+    private final String nomerRangka;
+    private final String nomerSTNK;
+
+    public PencocokanKendaraan(String NIK, String nomerMesin, String nomerRangka, String nomerSTNK){
+        this.NIK = NIK;
+        this.nomerMesin = nomerMesin;
+        this.nomerRangka = nomerRangka;
+        this.nomerSTNK = nomerSTNK;
+    }
+    public void PencocokanKendaraan(){
+        System.out.println("Proses pencocokan STNK: " + this.nomerSTNK );
+        System.out.println("Proses pencocokan NIK: " + this.NIK );
+
+    }
+    public boolean validasiData(){
+        // data asal;
+        String NiK = "012345";
+        String NomerMesin = "AB-123";
+        String NomerRangka = "BC-123";
+        String NomerSTNK = "CD-123";
+
+        if(this.NIK.equals(NiK) && this.nomerMesin.equals(NomerMesin) && this.nomerRangka.equals(NomerRangka) && this.nomerSTNK.equals(NomerSTNK)){
+            System.out.println("Proses validasi kendaraan berhasil");
+            return true;
+        } else {
+            System.out.println("Proses validasi kendaraan tidak berhasil");
+            return false;
+        }
+    }
+}
