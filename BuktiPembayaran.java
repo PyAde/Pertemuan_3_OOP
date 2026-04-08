179a7700238703ead7efe53f427dc9227a439066
PyAde <putraadelio26@gmail.com>
2026-04-08 19:45:18 +0800

    .

--- BuktiPembayaran.java
+++ BuktiPembayaran.java
@@ -1,9 +1,9 @@
 public class BuktiPembayaran {
     
-    // Atribut (Private)
-    private String tanggalPelunasan;
-    private Double totalDibayar;
-    private String statusPembayaran;
+
+    private final  String tanggalPelunasan;
+    private final  Double totalDibayar;
+    private final String statusPembayaran;
 
     // Constructor untuk inisialisasi objek
     public BuktiPembayaran(String tanggalPelunasan, Double totalDibayar, String statusPembayaran) {
@@ -12,7 +12,7 @@ public class BuktiPembayaran {
         this.statusPembayaran = statusPembayaran;
     }
 
-    // Method (Public)
+
     public void tampilkanBuktiPembayaran() {
         System.out.println("=== BUKTI PEMBAYARAN PAJAK ===");
         System.out.println("Tanggal Pelunasan : " + this.tanggalPelunasan);
@@ -22,16 +22,16 @@ public class BuktiPembayaran {
     }
 
     public void cetakPDF() {
-        // Logika teknis untuk mengonversi data struk menjadi file berekstensi .pdf
+
         System.out.println("Mencetak bukti pembayaran ke format PDF...");
     }
 
     public void kirimEmail() {
-        // Logika teknis untuk mengirimkan file/teks bukti ke email pengguna
+
         System.out.println("Mengirim bukti pembayaran via Email...");
     }
 
-    // Getter dan Setter (Standar Enkapsulasi)
+
     public String getTanggalPelunasan() { return tanggalPelunasan; }
     public Double getTotalDibayar() { return totalDibayar; }
     public String getStatusPembayaran() { return statusPembayaran; }
--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        // Data pengguna
         Login user1 = new Login("Adi", "user123");
+        PencocokanKendaraan transport_1 = new PencocokanKendaraan("012345", "AB-123", "BC-123", "CD-123");
 
         boolean loginBerhasil = user1.validasiLogin();
 
@@ -8,17 +10,14 @@ public class Main {
             System.err.println("Login atas nama " + user1.getUsername() + " berhasil");
         
             // Validasi kendaraan;
-            PencocokanKendaraan transport_1 = new PencocokanKendaraan("012345", "AB-123", "BC-123", "CD-123");
             boolean validasiBerhasil = transport_1.validasiData();
             if(validasiBerhasil){
                 System.out.println("Validasi kendaraan atas nama " + user1.getUsername() + "berhasil dilakukan");
                 // lanjut ke proses class diagram berikutnya 
+
             }else{
                 System.out.println("Validasi kendaraan atas nama" + user1.getUsername() + "tidak berhasil dilakukan");
             }
-            
-
-        
         user1.logout();
         }else{
             System.out.println("Username dan password yang anda masukan salah");
